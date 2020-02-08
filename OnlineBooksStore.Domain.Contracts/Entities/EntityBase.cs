72379286aee002e96c92e31dee5103b18b916d9f
EugeneSoz <evgeniy.sozonov@intourist.ru>
2020-02-08 14:14:33 +0300

    Add project files.

---
+++ OnlineBooksStore.Domain.Contracts/Entities/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineBooksStore.Domain.Contracts.Entities
+{
+    /// <summary>
+    /// Base entity class with the identifier
+    /// </summary>
+    public class EntityBase
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public long Id { get; set; }
+    }
+}
---
+++ OnlineBooksStore.Persistence.EF/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnlineBooksStore.Persistence.EF
+{
+    public class Class1
+    {
+    }
+}
